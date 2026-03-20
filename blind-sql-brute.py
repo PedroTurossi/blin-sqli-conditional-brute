93d495ef62bf8a16907c13df550c6b6548e35a68
Pedro H. Turossi <157643488+PedroTurossi@users.noreply.github.com>
2026-03-20 10:28:52 -0300

    Update blind-sql-brute.py

--- blind-sql-brute.py
+++ blind-sql-brute.py
@@ -7,7 +7,7 @@ link = 'https://{url}/'
 headers = { 
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36", 
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", 
-    "Referer": "https://{url}/" 
+    "Referer": link
 } 
 
 regex = r'Welcome back' 
